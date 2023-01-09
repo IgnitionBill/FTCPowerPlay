7934543db3ec4db8e1cdcbee07bd3bed155b7967
Bill Ignition <valus@hugelheim.fun>
2023-01-09 00:58:28 -0500

    FtcRobothController recognized the camera successfully.

--- TeamCode/src/main/cpp/ftcrobotcontroller.cpp
+++ TeamCode/src/main/cpp/ftcrobotcontroller.cpp
@@ -1,5 +1,6 @@
 #include <jni.h>
 #include <string>
+#include "include/librealsense2/rs.hpp"
 
 // Write C++ code here.
 //
@@ -31,4 +32,17 @@ JNIEXPORT jstring JNICALL
 Java_org_firstinspires_ftc_teamcode_CameraWrapper_cameraStringFromJNI(JNIEnv *env, jobject thiz) {
     std::string hello = "Hello from Camera C++";
     return env->NewStringUTF(hello.c_str());
+}
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_org_firstinspires_ftc_teamcode_CameraWrapper_nGetLibrealsenseVersionFromJNI(JNIEnv *env,
+                                                                                 jclass clazz) {
+    return (*env).NewStringUTF(RS2_API_VERSION_STR);
+}
+extern "C"
+JNIEXPORT jint JNICALL
+Java_org_firstinspires_ftc_teamcode_CameraWrapper_nGetCamerasCountFromJNI(JNIEnv *env,
+                                                                          jclass clazz) {
+    rs2::context ctx;
+    return ctx.query_devices().size();;
 }
