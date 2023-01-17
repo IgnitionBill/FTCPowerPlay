c49d59a91cd2daa3908844aed975c6183efe9eae
cratercruncher <cratercruncher@dragomight.us>
2023-01-16 22:31:51 -0500

    Updated Vuforia key and Android Manifest for FtcRobotController 8.1.1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaStreamOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaStreamOpMode.java
@@ -16,7 +16,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 public class VuforiaStreamOpMode extends LinearOpMode {
 
     // TODO: fill in
-    public static final String VUFORIA_LICENSE_KEY = "YOUR KEY HERE";
+    public static final String VUFORIA_LICENSE_KEY = "ARxtv8D/////AAABmZ4ts8AsuUsfv570CA0FvZY2KnfvNO/V97gOg+9/vwscYSkdGKDVInMDgmTdCEI2e8l96txPXrBEK8uLhdOrPHdG4KePbI++PmIY30U7WO62l9+6kVQiW1Dqkc/ddlh9X4RkOGiadErsSDHFuE8sea4IieU+42L9BnIWJIvm9FeoMIpakxvy/e3TnHks4ZbKVkdImRnScYAX3X34Z3FknB6K6LfXwpk2MdDGQuFrZh/2M7u84uzDfSXt+Ltpv+VGO1+yxWu/+6rpzSp/sE3OrIF48kmwwRLCh8ixInK4S0R4f1vAFpgN2MI+h20H50j/Zp2c2ppY52Dzpq2Mk+f9JiWo90003D9syuWyCsxKzPUV";;
 
     @Override
     public void runOpMode() throws InterruptedException {
