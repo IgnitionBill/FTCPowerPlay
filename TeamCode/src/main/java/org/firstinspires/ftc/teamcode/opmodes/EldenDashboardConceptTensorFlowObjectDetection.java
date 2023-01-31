b98a2b91ca2e78c72f7db251cfb262afa45f40c2
cratercruncher <cratercruncher@dragomight.us>
2023-01-31 09:16:04 -0500

    Elden Park League sleeve works!  Tested with Moto e5 camera.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/EldenDashboardConceptTensorFlowObjectDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/EldenDashboardConceptTensorFlowObjectDetection.java
@@ -65,7 +65,8 @@ public class EldenDashboardConceptTensorFlowObjectDetection extends LinearOpMode
      * has been downloaded to the Robot Controller's SD FLASH memory, it must to be loaded using loadModelFromFile()
      * Here we assume it's an Asset.    Also see method initTfod() below .
      */
-    private static final String TFOD_MODEL_ASSET = "PowerPlayEldenParking.tflite";
+    private static final String TFOD_MODEL_ASSET = "PowerPlayEldenParkLeague-v1.tflite";
+    //private static final String TFOD_MODEL_ASSET = "PowerPlayEldenParking.tflite";
     // private static final String TFOD_MODEL_FILE  = "/sdcard/FIRST/tflitemodels/CustomTeamModel.tflite";
 
     private static final String[] LABELS = {
